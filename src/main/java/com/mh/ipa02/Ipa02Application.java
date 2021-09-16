1d9b3fdc7776e5d2ffe69ab99fd70ef01f3161b2
kdypro11 <kdypro11@naver.com>
2021-09-16 09:37:18 +0900

    Initial commit

---
+++ src/main/java/com/mh/ipa02/Ipa02Application.java
@@ -0,0 +1,13 @@
+package com.mh.ipa02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ipa02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ipa02Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/mh/ipa02/Ipa02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mh.ipa02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ipa02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
